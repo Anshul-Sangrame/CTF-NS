b5ca6ccc422b3290df30d685b09c3418be602a38
Anshul-Sangrame <anshul.sangrame@gmail.com>
2024-04-21 13:38:20 +0530

    minor change

--- heartbleed.sh
+++ heartbleed.sh
@@ -1,1 +1,1 @@
-curl -s -N -X POST -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' -d "file=$1" 10.200.33.61:1624  
+curl -s -N -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d "file=$1" 10.200.33.61:1624  
